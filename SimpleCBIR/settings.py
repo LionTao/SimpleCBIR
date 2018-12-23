e94d69a511558be5cef96e2ec8f80951cc4e9bf8
LionTao <taojiachun980831@163.com>
2018-12-23 15:34:30 +0800

    modify settings

--- SimpleCBIR/settings.py
+++ SimpleCBIR/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC+8'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
