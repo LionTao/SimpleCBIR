1f790133aecd9e81dcbe4bc3a9315733d8c6cf92
Steve <stevehawk@qq.com>
2018-12-23 17:03:09 +0800

    Update urls.py

--- SimpleCBIR/urls.py
+++ SimpleCBIR/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from main import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('main.urls')),
+    path('', include('main.urls')),
 ]
